bcb3e36e30c18866b146ca627873f9c18ff7a9bd
Adriel <adriel.fsantos@outlook.com>
2023-08-09 23:33:00 -0300

    Adiciona importação de fontes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, useFonts } from '@expo-google-fonts/poppins';
+import './global.css';
+import OnBoarding from './src/screens/OnBoarding';
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold
+  });
+
+  if (!hasLoadedFonts) return null;
   return (
     <View className='flex-1 justify-center items-center'>
       <Text className=''>Open up App.tsx to start working on your app!</Text>
