d1a37246a48d1bb7f326f9a146ac658fff5cdb09
Adriel <adriel.fsantos@outlook.com>
2023-08-11 23:36:33 -0300

    Insere tela de Onboard em App

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, useFonts } from '@expo-google-fonts/poppins';
 import './global.css';
-import OnBoarding from './src/screens/OnBoarding';
+import Onboard from './src/screens/Onboard';
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -11,10 +11,7 @@ export default function App() {
 
   if (!hasLoadedFonts) return null;
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className=''>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Onboard />
   );
 }
 
