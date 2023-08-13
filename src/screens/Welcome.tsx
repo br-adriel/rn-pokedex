13904bb0e6b8d37a6261ab1139217fbe31babcf8
Adriel <adriel.fsantos@outlook.com>
2023-08-12 21:36:36 -0300

    Adiciona tela Welcome

---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,32 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { SafeAreaView, StatusBar, TouchableOpacity, View } from 'react-native'
+import ActionButton from '../components/ActionButton'
+import OnboardItem from '../components/OnboardItem'
+import { TextMedium, TextSemibold } from '../components/Text'
+
+export default function Welcome() {
+  return (
+    <>
+      <StatusBar translucent backgroundColor='transparent' barStyle='dark-content' />
+      <SafeAreaView style={{ paddingTop: (StatusBar.currentHeight || 0) }} className='flex-1'>
+        <TouchableOpacity className='flex-row self-end items-center m-4' activeOpacity={0.7}>
+          <TextMedium className='text-lg mr-[6]'>Pular</TextMedium>
+          <MaterialCommunityIcons name='arrow-right' size={24} />
+        </TouchableOpacity>
+        <OnboardItem
+          title='Está pronto para essa aventura?'
+          description='Basta criar uma conta e começar a explorar o mundo dos Pokémon hoje!'
+          image={require('../../assets/img/characters/trainers.png')}
+        />
+        <View className='px-4 pb-4 gap-1'>
+          <ActionButton>
+            <TextSemibold className='text-white text-lg text-center'>Criar conta</TextSemibold>
+          </ActionButton>
+          <ActionButton variant='secondary'>
+            <TextSemibold className='text-action text-lg text-center'>Já tenho uma conta</TextSemibold>
+          </ActionButton>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
