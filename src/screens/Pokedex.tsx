a5b47e0a29aa0c3d0f3b2caf4ee6d33b726205bd
Adriel <adriel.fsantos@outlook.com>
2023-09-21 17:07:21 -0300

    Adiciona tela Pokedex

---
+++ src/screens/Pokedex.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { StatusBar, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInput from '../components/SearchInput'
+
+export default function Pokedex() {
+  const [busca, setBusca] = useState('')
+
+  return (
+    <>
+      <StatusBar backgroundColor='#ffffff' />
+      <SafeAreaView className='flex-1 bg-white'>
+        <View className='w-screen p-4'>
+          <SearchInput
+            placeholder='Procurar Pokémon...'
+            value={busca}
+            onChangeText={text => setBusca(text)}
+            onSubmitEditing={() => { console.log(busca); setBusca('') }}
+          />
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
